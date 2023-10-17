a386c1e590b08943030738915083b8a31baed5eb
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-17 22:20:11 +0200

    feat: :sparkles: implement local storage

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,15 @@ const form = document.querySelector("#grocery-form");
 const input = document.querySelector("#grocery-input");
 const addButton = form.querySelector("button");
 const list = document.querySelector("#grocery-list");
+const clearButton = document.querySelector("#clear-list");
+const filterInput = document.querySelector("#filter");
 // Koppla händelser till elementen
 // Submit-händelsen på forumlär
 form.addEventListener("submit", onSaveGrocery);
+clearButton.addEventListener("click", onClearList);
+list.addEventListener("click", onClickRemoveGrocery);
+filterInput.addEventListener("input", onFilterGroceries);
+document.addEventListener("DOMContentLoaded", onDisplayGroceries);
 // Spara inköp till listan
 function onSaveGrocery(e) {
     e.preventDefault();
@@ -14,6 +20,7 @@ function onSaveGrocery(e) {
     if (grocery.trim().length > 0) {
         // Lägg till i listan
         addGroceryToDOM(grocery);
+        addToStorage(grocery);
     }
     input.value = "";
 }
@@ -39,4 +46,62 @@ function createIcon(classes) {
     const icon = document.createElement("i");
     icon.className = classes;
     return icon;
+}
+function onClearList() {
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+    localStorage.removeItem("groceries");
+}
+function onClickRemoveGrocery(e) {
+    var _a;
+    const target = e.target;
+    console.log("target", target.parentElement.parentElement);
+    if ((_a = target.parentElement) === null || _a === void 0 ? void 0 : _a.classList.contains("btn-remove")) {
+        const item = target.parentElement.parentElement;
+        // item.remove();
+        removeGrocery(item);
+    }
+}
+function removeGrocery(item) {
+    item.remove();
+    removeFromStorage(item.textContent);
+}
+// this refererar till aktiv instans
+function onFilterGroceries() {
+    var _a, _b;
+    const value = this.value.toLowerCase();
+    const groceries = document.querySelectorAll("li");
+    for (let grocery of groceries) {
+        const item = (_b = (_a = grocery.firstChild) === null || _a === void 0 ? void 0 : _a.textContent) === null || _b === void 0 ? void 0 : _b.toLowerCase();
+        if ((item === null || item === void 0 ? void 0 : item.indexOf(value)) !== -1) {
+            grocery.style.display = "flex";
+        }
+        else {
+            grocery.style.display = "none";
+        }
+    }
+}
+function addToStorage(grocery) {
+    // Hämta ut listan om den finns i localstorage
+    const groceries = getFromStorage();
+    // Om den finns lägg till grocery
+    groceries.push(grocery);
+    // Spara listan igen till localstorage
+    localStorage.setItem("groceries", JSON.stringify(groceries));
+}
+function getFromStorage() {
+    var _a;
+    let items;
+    items = (_a = JSON.parse(localStorage.getItem("groceries"))) !== null && _a !== void 0 ? _a : [];
+    return items;
+}
+function removeFromStorage(grocery) {
+    let groceries = getFromStorage();
+    groceries = groceries.filter((item) => item !== grocery);
+    localStorage.setItem("groceries", JSON.stringify(groceries));
+}
+function onDisplayGroceries() {
+    const groceries = getFromStorage();
+    groceries.forEach((grocery) => addGroceryToDOM(grocery));
 }
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,16 @@ const form = document.querySelector("#grocery-form") as HTMLFormElement;
 const input = document.querySelector("#grocery-input") as HTMLInputElement;
 const addButton = form.querySelector("button");
 const list = document.querySelector("#grocery-list") as HTMLUListElement;
+const clearButton = document.querySelector("#clear-list") as HTMLButtonElement;
+const filterInput = document.querySelector("#filter") as HTMLInputElement;
 
 // Koppla händelser till elementen
 // Submit-händelsen på forumlär
 form.addEventListener("submit", onSaveGrocery);
+clearButton.addEventListener("click", onClearList);
+list.addEventListener("click", onClickRemoveGrocery);
+filterInput.addEventListener("input", onFilterGroceries);
+document.addEventListener("DOMContentLoaded", onDisplayGroceries);
 
 // Spara inköp till listan
 function onSaveGrocery(e: SubmitEvent) {
@@ -17,6 +23,7 @@ function onSaveGrocery(e: SubmitEvent) {
   if (grocery.trim().length > 0) {
     // Lägg till i listan
     addGroceryToDOM(grocery);
+    addToStorage(grocery);
   }
   input.value = "";
 }
@@ -39,11 +46,74 @@ function createIconButton(classes: string): HTMLButtonElement {
   return button;
 }
 
-
 // implicit return gör att det fungerar, men är bra att vara övertydlige
 // så kan lägga till :HTMLElement
 function createIcon(classes: string): HTMLElement {
   const icon = document.createElement("i");
   icon.className = classes;
   return icon;
+}
+
+function onClearList(): void {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+  localStorage.removeItem("groceries");
+}
+
+function onClickRemoveGrocery(e: UIEvent): void {
+  const target = e.target as HTMLElement;
+  console.log("target", target!.parentElement!.parentElement);
+  if (target.parentElement?.classList.contains("btn-remove")) {
+    const item = target.parentElement!.parentElement as HTMLLIElement;
+    // item.remove();
+    removeGrocery(item);
+  }
+}
+
+function removeGrocery(item: HTMLLIElement): void {
+  item.remove();
+  removeFromStorage(item.textContent!);
+}
+
+// this refererar till aktiv instans
+function onFilterGroceries(this: HTMLInputElement): void {
+  const value = this.value.toLowerCase();
+  const groceries = document.querySelectorAll("li");
+
+  for (let grocery of groceries) {
+    const item = grocery.firstChild?.textContent?.toLowerCase();
+
+    if (item?.indexOf(value) !== -1) {
+      grocery.style.display = "flex";
+    } else {
+      grocery.style.display = "none";
+    }
+  }
+}
+
+function addToStorage(grocery: string): void {
+  // Hämta ut listan om den finns i localstorage
+  const groceries = getFromStorage();
+  // Om den finns lägg till grocery
+  groceries.push(grocery);
+  // Spara listan igen till localstorage
+  localStorage.setItem("groceries", JSON.stringify(groceries));
+}
+
+function getFromStorage(): string[] {
+  let items: [];
+  items = JSON.parse(localStorage.getItem("groceries")!) ?? [];
+  return items;
+}
+
+function removeFromStorage(grocery: string): void {
+  let groceries: string[] = getFromStorage();
+  groceries = groceries.filter((item) => item !== grocery);
+  localStorage.setItem("groceries", JSON.stringify(groceries));
+}
+
+function onDisplayGroceries(): void {
+  const groceries = getFromStorage();
+  groceries.forEach((grocery) => addGroceryToDOM(grocery));
 }
